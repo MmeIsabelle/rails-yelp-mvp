b2fae721cdedbdbe62e966dfd3a052ddd4bdff4b
Isabelle Lavoie <isabelle@kaastor.ca>
2021-08-11 11:29:48 -0700

    added the new and create actions and view with form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,16 +1,21 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:new, :create, :show]
+  before_action :set_restaurant, only: [:show]
   
   def index
     @restaurants = Restaurant.all
   end
 
   def new
-    
+    @restaurant = Restaurant.new
   end
 
   def create
-    
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def show
@@ -20,5 +25,9 @@ class RestaurantsController < ApplicationController
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
   end
 end
